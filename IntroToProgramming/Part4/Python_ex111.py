02befa0e74b2134901fb0e611797483abfc1d3e6
Zhanibek <smagul.zhanibek@gmail.com>
2018-08-16 12:07:05 +0600

    Example 111. Lines of documentation (Google docstyle).

---
+++ IntroToProgramming/Part4/Python_ex111.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    """Add two numbers.
+
+    Args:
+        x (int): The first integer number.
+        y (int): The second integer number.
+
+    Params:
+        :param x
+        :param y
+
+    Returns:
+        int: The return sum of two numbers. 
+    """
+    return x + y
