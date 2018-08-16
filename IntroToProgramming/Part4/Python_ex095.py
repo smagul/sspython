a9dd4e3315a1a892ad277107e6bd7c7dff31b02e
Zhanibek <smagul.zhanibek@gmail.com>
2018-08-16 10:04:46 +0600

    Example 94-96. Multiple use of functions

---
+++ IntroToProgramming/Part4/Python_ex095.py
@@ -0,0 +1,20 @@
+n = input("Input a number: ")
+n = int(n)
+if n % 2 == 0:
+    print("n - even.")
+else:
+    print("n - odd.")
+
+n = input("Input a number: ")
+n = int(n)
+if n % 2 == 0:
+    print("n - even.")
+else:
+    print("n - odd.")
+
+n = input("Input a number: ")
+n = int(n)
+if n % 2 == 0:
+    print("n - even.")
+else:
+    print("n - odd.")
---
+++ IntroToProgramming/Part4/Python_ex096.py
@@ -0,0 +1,13 @@
+def even_odd():
+    n = input("Input a number: ")
+    n = int(n)
+
+    if n % 2 == 0:
+        print("n - even.")
+    else:
+        print("n - odd.")
+
+
+even_odd()
+even_odd()
+even_odd()
